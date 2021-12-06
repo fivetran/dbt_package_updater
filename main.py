45396978dc12593c1f81c4948cde781e2dc080bf
Dylan Baker <dylan@dbanalytics.co>
2021-12-06 13:23:52 +0000

    first changes for v1.0.0 migration

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def update_packages(
 
         repo.update_file(
             path=packages_content.path,
-            message="Updating package dependendcies",
+            message="[ci skip] Updating package dependendcies",
             content=ruamel.yaml.dump(packages, Dumper=ruamel.yaml.RoundTripDumper),
             sha=packages_content.sha,
             branch=branch_name,
@@ -105,9 +105,44 @@ def update_project(
     new_version = ".".join(current_version_split)
     project["version"] = new_version
 
+    # v1.0.0 migrations
+
+    clean_targets = project.get("clean-targets", None)
+    if clean_targets:
+        project["clean-targets"].append("dbt_packages")
+
+    project.pop("source-paths", None)
+    project.pop("data-paths", None)
+
     repo.update_file(
         path=project_content.path,
-        message="Updating require-dbt-version",
+        message="[ci skip] Updating dbt_project.yml",
+        content=ruamel.yaml.dump(project, Dumper=ruamel.yaml.RoundTripDumper),
+        sha=project_content.sha,
+        branch=branch_name,
+    )
+
+
+def update_integration_project(
+    repo: github.Repository.Repository, branch_name: str, config: str
+) -> None:
+    project_content = repo.get_contents("integration_tests/dbt_project.yml")
+    project = ruamel.yaml.load(
+        project_content.decoded_content,
+        Loader=ruamel.yaml.RoundTripLoader,
+        preserve_quotes=True,
+    )
+
+    clean_targets = project.get("clean-targets", None)
+    if clean_targets:
+        project["clean-targets"].append("dbt_packages")
+
+    project.pop("source-paths", None)
+    project.pop("data-paths", None)
+
+    repo.update_file(
+        path=project_content.path,
+        message="[ci skip] Updating integration_tests/dbt_project.yml",
         content=ruamel.yaml.dump(project, Dumper=ruamel.yaml.RoundTripDumper),
         sha=project_content.sha,
         branch=branch_name,
@@ -124,7 +159,7 @@ def update_requirements(
             new_content += f"{requirement['name']}=={requirement['version']}\n"
         repo.update_file(
             path=requirements_content.path,
-            message="Updating dbt version in requirements.txt",
+            message="[ci skip] Updating dbt version in requirements.txt",
             content=new_content,
             sha=requirements_content.sha,
             branch=branch_name,
@@ -132,8 +167,73 @@ def update_requirements(
     except github.GithubException:
         repo.create_file(
             path="integration_tests/requirements.txt",
-            message="Updating dbt version in requirements.txt",
+            message="[ci skip] Updating dbt version in requirements.txt",
             content=new_content,
+            branch=branch_name,
+        )
+
+
+def update_data_folder_to_seed(
+    repo: github.Repository.Repository, branch_name: str
+) -> None:
+    try:
+        seed_files = repo.get_contents("data")
+    except:
+        seed_files = None
+    if seed_files:
+        for file in seed_files:
+            repo.create_file(
+                path=file.path.replace("data/", "seeds/"),
+                message="[ci skip] Moving folder",
+                content=file.decoded_content,
+                branch=branch_name,
+            )
+            repo.delete_file(
+                path=file.path,
+                message="[ci skip] Moving folder",
+                sha=file.sha,
+                branch=branch_name,
+            )
+    try:
+        integration_seed_files = repo.get_contents("integration_tests/data")
+    except:
+        integration_seed_files = None
+    if integration_seed_files:
+        for file in integration_seed_files:
+            repo.create_file(
+                path=file.path.replace("data/", "seeds/"),
+                message="[ci skip] Moving folder",
+                content=file.decoded_content,
+                branch=branch_name,
+            )
+            repo.delete_file(
+                path=file.path,
+                message="[ci skip] Moving folder",
+                sha=file.sha,
+                branch=branch_name,
+            )
+
+
+def add_dbt_packages_to_gitnore(
+    repo: github.Repository.Repository, branch_name: str
+) -> None:
+    try:
+        gitignore_content = repo.get_contents(".gitignore")
+        gitignore = gitignore_content.decoded_content.decode("utf-8")
+        gitignore += "\n"
+        gitignore += "dbt_packages/"
+        repo.update_file(
+            path=gitignore_content.path,
+            message="[ci skip] Adding dbt_packages to .gitignore",
+            content=gitignore,
+            sha=gitignore_content.sha,
+            branch=branch_name,
+        )
+    except github.GithubException:
+        repo.create_file(
+            path=".gitignore",
+            message="[ci skip] Adding dbt_packages to .gitignore",
+            content="dbt_packages/",
             branch=branch_name,
         )
 
@@ -181,6 +281,8 @@ def main():
         update_packages(repo, branch_name, config)
         update_project(repo, branch_name, config)
         update_requirements(repo, branch_name, config)
+        add_dbt_packages_to_gitnore(repo, branch_name)
+        update_data_folder_to_seed(repo, branch_name)
         open_pull_request(repo, branch_name, default_branch)
 
 
