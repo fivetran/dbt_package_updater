a0da9eb73f4c42b1f8265050ee35915380d89d43
Frydafly <fryda.guedes@gmail.com>
2023-02-21 12:21:09 -0600

    removed to do that was done

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import hashlib
 import time
 import ruamel.yaml
 
-#TODO have the PR body tag the repo owner and contributors? - https://docs.github.com/en/rest/reference/repos#list-repository-contributors
 #TODO trigger this workflow after new dbt-arc-functions are released, and get latest revision from dbt-arc-functions repo
 #TODO add a check to see if the dbt version is already the latest version
 #TODO add a check to see if the package versions are already the latest version
