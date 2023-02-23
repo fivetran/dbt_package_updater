3f377a6ed90ed839a1a1482d90741d3240a5050a
Frydafly <fryda.guedes@gmail.com>
2023-02-23 13:32:49 -0600

    fixed line length

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-"""This script updates the dbt version and package versions in all dbt projects in the bsd organization."""
+"""This script updates the dbt version and package versions in all
+    dbt projects in the bsd organization."""
 
 import time
 import hashlib
@@ -8,10 +9,10 @@ import yaml
 
 import ruamel.yaml
 
-# TODO trigger this workflow after new dbt-arc-functions are released, and get 
+# TODO trigger this workflow after new dbt-arc-functions are released, and get
 # latest revision from dbt-arc-functions repo
 # TODO add a check to see if the dbt version is already the latest version
-# TODO add a check to see if the package versions are 
+# TODO add a check to see if the package versions are
 # already the latest version
 # TODO add a step that pulls latest dbt-arc-functions revision
 
@@ -20,11 +21,11 @@ def set_branch_name() -> str:
     """Generates a unique branch name for the pull request."""
     hash_name = hashlib.sha1()
     hash_name.update(str(time.time()).encode("utf-8"))
-    return f"MagicBot_{hash.hexdigest()[:10]}"   
+    return f"MagicBot_{hash.hexdigest()[:10]}"  # noqa: F821 
 
 
 def load_credentials() -> dict:
-    """Loads credentials from github settings."""                     
+    """Loads credentials from github settings."""                   
     with open("credentials.yml", encoding='utc-8') as file:
         creds = yaml.load(file, Loader=yaml.FullLoader)
     return creds
@@ -57,7 +58,9 @@ def setup_repo(client: github.Github, repo_name: str, branch_name: str):
     return repo, default_branch
 
 
-def update_packages(repo: github.Repository.Repository, branch_name: str, config: dict) -> None:
+def update_packages(
+        repo: github.Repository.Repository, branch_name: str, config: dict
+            ) -> None:
     """Updates the packages.yml file."""
     try:
         packages_content = repo.get_contents("packages.yml")
